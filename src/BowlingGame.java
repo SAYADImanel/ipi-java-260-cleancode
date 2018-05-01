5b59d1b171edbcdc1e40f3ecd3d0e023edece757
LAPTOP-97H7B5O5\manel <manel.sayadi@yahoo.fr>
2018-05-01 17:25:32 +0200

    TP bowling

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -1,12 +1,58 @@
 public class BowlingGame {
 
-    private int score;
+    private int[] rolls = new int[21];
+    private int currentRoll;
 
     public int score() {
+        int score = 0;
+        int i = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            int quille1 = i;
+            int quille2 = i + 1;
+            if (rolls[quille1] == 10) { //strike
+                int quilleBonus1 = quille1 + 1;
+                int quilleBonus2 = quille1 + 2;
+                score += 10 + rolls[quilleBonus1] + rolls[quilleBonus2];
+                i++;
+            } else if (rolls[quille1] + rolls[quille2] == 10) { //spare
+                int quilleBonus1 = quille2 + 1;
+                score += 10 + rolls[quilleBonus1];
+                i += 2;
+            } else {
+                score += rolls[quille1] + rolls[quille2];
+                i += 2;
+            }
+            
+            
+            
+        }
         return score;
-    }
+        
+       }
+    
+    
 
-    public void roll(int numberPins) {
-        score += numberPins;
+    public void roll(int numberPins) throws CoupInvalideException {
+    	if (numberPins<0 || numberPins>10 ) {
+    		throw new CoupInvalideException ();
+
+        	 }
+    	
+    	
+    	if ((currentRoll % 2) == 1) {
+    		
+    		  int lanceAvant = rolls[currentRoll-1];
+
+    		if (lanceAvant+numberPins>10) {
+	    		throw new CoupInvalideException ();
+	    	}
+    		
+    	}
+    	
+    	
+    	
+    	//if (rolls[currentRoll].numberPins + rolls.currentRoll++.numberPins>10)
+    	
+        rolls[currentRoll++] = numberPins;
     }
 }
--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -1,9 +1,16 @@
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
 
 
 public class BowlingGameTest {
+	
+	//frame comprend 10 lancers
+	//fait tomber 10 : strike
+	//fait tomber 10 en 2 lancers : spare
+	//si on a un strike alors il reste 9 frames.
 
     BowlingGame game = new BowlingGame();
 
@@ -15,23 +22,115 @@ public class BowlingGameTest {
 
     @Test
     public void uneSeuleQuille(){
-        game.roll(1);
+    	//1 lancer et 1 seule quille
+    	game.roll(1);
+        //19 lancers avec 0 quilles
         rollMany(19, 0);
         assertEquals(1, game.score());
     }
 
     @Test
     public void bonusDuSpare(){
+    	//1 lancer et 8 quilles
         game.roll(8);
+        //1 lancer et 2 quilles
         game.roll(2);
+        //1 lancer et une quille
         game.roll(1);
+        //17 lancers avec 0 quille
         rollMany(17, 0);
+        //normalement 11 mais on ajoute 1 du troisieme lancer donne 12.
         assertEquals(12, game.score());
     }
+    
+    @Test
+    public void doubleStrike() {
+    game.roll(10);
+   
+   game.roll(10);
+    
+    game.roll(5);
+    game.roll(4);
+    rollMany(14,0);
+    assertEquals(53,game.score());
+    	
+    	
+    	
+    }
+    
+    // spare  bonus : ajoute le score du prochain lancer.
+    
+    @Test
+    public void deuxQuilles() {
+    game.roll(2);
+    rollMany(19, 0);
+    assertEquals(2, game.score());
+    
+    }
+    
+    
+    @Test
+    public void PlusqueDixQuilles() {
+    CoupInvalideException plus = null;
+    try {
+    game.roll(15);
+    }
+    
+    catch (CoupInvalideException dix) {
+    plus = dix;
+    }
+    
+    assertNotNull(plus);
+    	
+    	
+    }
+    
+    @Test
+    public void nombreQuillesNegatif() {
+    CoupInvalideException negatif = null;
+    
+  try {
+    game.roll(-1);
+   } 
+    
+    catch (CoupInvalideException erreur) {
+    
+    negatif = erreur;	
+    
+    }
+   
+  	assertNotNull(negatif);
+    	
+    }
+    
+   
+    @Test
+    public void deuxLances() {
+    CoupInvalideException nombre = null;
+    try {
+        game.roll(7);
+        game.roll(6);
+       } 
+    catch (CoupInvalideException erreur1) {
+        
+        nombre = erreur1;	
+        
+        }
+    assertNotNull(nombre);
+    	
+    }
 
+    
+    
     private void rollMany(int numberRolls, int numberPins) {
         for (int i = 0; i < numberRolls; i++) {
             game.roll(numberPins);
         }
+        
+        //plus que 10 quilles est une errure
     }
+ 
+    
+    
+    
 }
